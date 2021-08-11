9d98903279246051ba4e53c8ecd2bd138cb27be5
Paulo Lieuthier <paulolieuthier@gmail.com>
2021-08-11 17:47:55 -0300

    feat(operator): set resources for runner and dind containers

--- operator/api/v1alpha1/zz_generated.deepcopy.go
+++ operator/api/v1alpha1/zz_generated.deepcopy.go
@@ -320,7 +320,13 @@ func (in *ActionsRunnerSpec) DeepCopyInto(out *ActionsRunnerSpec) {
 			(*in)[i].DeepCopyInto(&(*out)[i])
 		}
 	}
-	in.Resources.DeepCopyInto(&out.Resources)
+	if in.Resources != nil {
+		in, out := &in.Resources, &out.Resources
+		*out = make(map[string]v1.ResourceRequirements, len(*in))
+		for key, val := range *in {
+			(*out)[key] = *val.DeepCopy()
+		}
+	}
 	if in.Affinity != nil {
 		in, out := &in.Affinity, &out.Affinity
 		*out = new(v1.Affinity)
