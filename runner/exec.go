bcad2a9b2392cdc6682c52917bb66ef3fe65e43b
Paulo Lieuthier <paulolieuthier@gmail.com>
2021-10-06 10:21:06 -0300

    feat(runner): ignore errors from sending signals to subprocesses

--- runner/exec.go
+++ runner/exec.go
@@ -17,6 +17,7 @@ func run(command string, args ...string) chan error {
 	cmd.SysProcAttr = &syscall.SysProcAttr{Pdeathsig: syscall.SIGINT}
 
 	ret := make(chan error)
+	running := make(chan error)
 
 	if err := cmd.Start(); err != nil {
 		ret <-errors.Wrapf(err, "Error starting command [%s %s]", command, args)
@@ -26,15 +27,21 @@ func run(command string, args ...string) chan error {
 	signals := make(chan os.Signal, 1)
 	go func() {
 		for sig := range signals {
-			if err := cmd.Process.Signal(sig); err != nil {
-				logger.Printf("Error sending signal for command [%s %s]: %v\n", command, args, err)
+			select {
+			case <-running:
+				return
+			default:
+				break
 			}
+			cmd.Process.Signal(sig)
 		}
 	}()
 	signal.Notify(signals)
 
 	go func() {
-		ret <- cmd.Wait()
+		err := cmd.Wait()
+		ret <- err
+		running <- err
 	}()
 
 	return ret
